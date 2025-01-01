6e65d9cc76cde26b9cc722287936e52d1dc41dbc
CaiquePrado <caiqueprado76@gmail.com>
2025-01-01 15:03:21 -0300

    feat: config requests specifications

---
+++ src/main/java/spec/ServerSpecification.java
@@ -0,0 +1,30 @@
+package spec;
+
+import static io.restassured.filter.log.LogDetail.ALL;
+import static io.restassured.http.ContentType.JSON;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import utils.PropertiesUtils;
+
+@NoArgsConstructor(access =  AccessLevel.PRIVATE)
+public class ServerSpecification {
+  
+  public static final String VIACEP_URI = "viacep-baseuri";
+
+  public static RequestSpecBuilder requestSpecBuilder(){
+    return new RequestSpecBuilder()
+    .setContentType(JSON)
+    .setAccept(JSON)
+    .log(ALL);
+  }
+
+  public static RequestSpecification getRequestSpecification(){
+    return requestSpecBuilder()
+    .setAccept(JSON)
+    .setBaseUri(PropertiesUtils.getBaseURI(VIACEP_URI))
+    .build();
+  }
+}
