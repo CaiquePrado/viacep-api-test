6ff7d97b7ed11de1107dd4ac4a48c13231cbcdf3
CaiquePrado <caiqueprado76@gmail.com>
2025-01-02 18:43:48 -0300

    feat: add endpoint for healthcheck

---
+++ src/main/java/constants/EndpointConstants.java
@@ -0,0 +1,9 @@
+package constants;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access =  AccessLevel.PRIVATE)
+public class EndpointConstants {
+  public static final String HTTP_200_UP = "/01001000/json/";
+}
