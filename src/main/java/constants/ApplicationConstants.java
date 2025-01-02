ed3bad55f61380c6b30a98ea1584a67367dd3cd6
CaiquePrado <caiqueprado76@gmail.com>
2025-01-02 19:49:30 -0300

    feat: add news constants

--- src/main/java/constants/ApplicationConstants.java
+++ src/main/java/constants/ApplicationConstants.java
@@ -5,6 +5,12 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access =  AccessLevel.PRIVATE)
 public class ApplicationConstants {
+
   public static final String ENV = "env";
   public static final String UAT = "uat";
+
+  public static final String VALID_CEP = "01001000";
+
+  public static final String SCHEMAS = "src/test/resources/schemas/";
+  public static final String GET_VALID_CEP_CONTRACT_JSON = "GET-valid-cep.json";
 }
--- src/main/java/constants/EndpointConstants.java
+++ src/main/java/constants/EndpointConstants.java
@@ -5,5 +5,7 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access =  AccessLevel.PRIVATE)
 public class EndpointConstants {
-  public static final String HTTP_200_UP = "/01001000/json/";
+  public static final String HTTP_200_UP = "/01001000/json/"; 
+  public static final String CEP_BY_ID = "/{cep}/json/";
+
 }
