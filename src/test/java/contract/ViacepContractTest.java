b12d771c514beefc6d4b3f225e4656daa918655d
CaiquePrado <caiqueprado76@gmail.com>
2025-01-07 19:44:35 -0300

    test: add get by address contract

--- src/test/java/contract/ViacepContractTest.java
+++ src/test/java/contract/ViacepContractTest.java
@@ -1,8 +1,6 @@
 package contract;
 
-import static constants.ApplicationConstants.GET_VALID_CEP_BUT_NONE_EXISTENT_JSON;
-import static constants.ApplicationConstants.GET_VALID_CEP_CONTRACT_JSON;
-import static constants.ApplicationConstants.SCHEMAS;
+import static constants.ApplicationConstants.*;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchema;
 import static org.apache.http.HttpStatus.SC_OK;
 
@@ -28,5 +26,13 @@ public class ViacepContractTest extends BaseTest {
     .getValidButNonexistentCep()
     .statusCode(SC_OK)
     .body(matchesJsonSchema(new File(SCHEMAS + GET_VALID_CEP_BUT_NONE_EXISTENT_JSON )));
+  }
+
+  @Test
+  public void validCepByAddressContract(){
+    viacepClient
+    .getCepByAddress()
+    .statusCode(SC_OK)
+    .body(matchesJsonSchema(new File(SCHEMAS + GET_VALID_CEP_BY_ADDRESS_JSON)));
   }
 }
