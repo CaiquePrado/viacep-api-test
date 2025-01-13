c71c449eb6f236cab131fa146e2e0788752d0643
CaiquePrado <caiqueprado76@gmail.com>
2025-01-13 19:35:11 -0300

    feat: add assertions for invalidCepTest

--- src/test/java/functional/ViacepFunctionalTest.java
+++ src/test/java/functional/ViacepFunctionalTest.java
@@ -2,11 +2,17 @@ package functional;
 
 import static org.apache.http.HttpStatus.SC_BAD_REQUEST;
 import static org.apache.http.HttpStatus.SC_OK;
+import static org.hamcrest.Matchers.containsString;
+import static org.testng.Assert.assertEquals;
 import static org.hamcrest.Matchers.equalTo;
+import responses.ErrorMessage;
 
+
+import factory.ErrorMessageFactory;
 import org.testng.annotations.Test;
 
 import client.BaseTest;
+import responses.Address;
 
 public class ViacepFunctionalTest extends BaseTest {
   
@@ -14,24 +20,22 @@ public class ViacepFunctionalTest extends BaseTest {
   public void ValidCepTest(){
     viacepClient
     .getCepById()
-    .statusCode(SC_OK);
-    // .extract()
-    // .body()
-    // .as(Address.class);
-
-    //TODO: Extrair como classe Adress para fazer assertions. https://github.com/CaiquePrado/desafio-viacep/blob/main/src/test/java/dbserver/desafioviacep/desafioviacep/ConsultaCepTest.java
-    //TODO: Fazer teste de ValidCepTest com provider para usar mais ceps e com mais diferencas que são certos. Ex: 01001-000 e 01001000.
-    //TODO:Adicionar description no @Test referentes aos cenários de teste.
-    //TODO:Adicionar logs em todo processo.
-    //TODO:Implementar allure reports.
-    //TODO:Mudar nome dos testes para padão Deve.
+    .statusCode(SC_OK)
+    .extract()
+    .body()
+    .as(Address.class);
   }
 
   @Test
   public void invalidCepTest(){
+    ErrorMessage expectedError = ErrorMessageFactory.badRequestErrorMessage();
+
     viacepClient
     .getInvalidCepById()
-    .statusCode(SC_BAD_REQUEST);
+    .statusCode(SC_BAD_REQUEST)
+    .body(containsString(expectedError.getStatus()))
+    .body(containsString(expectedError.getTitle()))
+    .body(containsString(expectedError.getDetails()));
   }
 
   @Test
@@ -66,21 +70,21 @@ public class ViacepFunctionalTest extends BaseTest {
   @Test
   public void CepByAddressWithoutUFTest(){
     viacepClient
-    .getCepByAddressWhithoutUF()
+    .getCepByAddressWithoutUF()
     .statusCode(SC_BAD_REQUEST);
   }
 
   @Test
   public void CepByAddressWithoutCityTest(){
     viacepClient
-    .getCepByAddressWhithoutCity()
+    .getCepByAddressWithoutCity()
     .statusCode(SC_BAD_REQUEST);
   }
 
   @Test
   public void CepByAddressInvalidCityTest(){
     viacepClient
-    .getCepByAddressWhithoutCity()
+    .getCepByAddressWithoutCity()
     .statusCode(SC_BAD_REQUEST);
   }
 
@@ -94,7 +98,15 @@ public class ViacepFunctionalTest extends BaseTest {
   @Test
   public void CepByAddressWithoutStreetTest(){
     viacepClient
-    .getCepByAddressWhithoutStreet()
+    .getCepByAddressWithoutStreet()
     .statusCode(SC_BAD_REQUEST);
   }
 }
+
+
+//TODO: Extrair como classe Adress para fazer assertions. https://github.com/CaiquePrado/desafio-viacep/blob/main/src/test/java/dbserver/desafioviacep/desafioviacep/ConsultaCepTest.java
+//TODO: Fazer teste de ValidCepTest com provider para usar mais ceps e com mais diferencas que são certos. Ex: 01001-000 e 01001000.
+//TODO:Adicionar description no @Test referentes aos cenários de teste.
+//TODO:Adicionar logs em todo processo.
+//TODO:Implementar allure reports.
+//TODO:Mudar nome dos testes para padão Deve.
