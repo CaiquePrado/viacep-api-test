efa225b47f37b9d3c96ebe235968f77059df02b6
CaiquePrado <caiqueprado76@gmail.com>
2025-01-13 19:34:37 -0300

    feat: add ErrorMessage model and factory

---
+++ src/main/java/factory/ErrorMessageFactory.java
@@ -0,0 +1,15 @@
+package factory;
+
+import responses.ErrorMessage;
+
+public interface ErrorMessageFactory {
+
+    static ErrorMessage badRequestErrorMessage(){
+        return ErrorMessage
+        .builder()
+        .title("Verifique a URL")
+        .status("Http 400")
+        .details("{Bad Request}")
+        .build();
+    }
+}
---
+++ src/main/java/responses/ErrorMessage.java
@@ -0,0 +1,11 @@
+package responses;
+
+import lombok.*;
+
+@Data
+@Builder
+public class ErrorMessage {
+    private String title;
+    private String status;
+    private String details;
+}
